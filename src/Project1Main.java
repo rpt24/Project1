77bf196ddaed8c468a5a82f2647521e2574c7423
rptho <rptho@DESKTOP-K3GT2FD>
2020-03-22 12:27:55 -0500

    final cut. t test added. comments added.

--- src/Project1Main.java
+++ src/Project1Main.java
@@ -1,24 +1,41 @@
+/************************************************************************* 
+  Student Name: Ryan Thornton
+  File Name: Project1Main.java
+  Project 1
+
+  This file is the main file that drives the program. It reads in the
+ data file Scores.txt and passes the data into a constructor for the 
+ Scores class. It then drives the new Scores object to call its
+ calculate functions and lastly print the data
+**************************************************************************/
 import java.io.*;
 
 public class Project1Main 
 {
-
+	
+	/*
+	 * This is the main function. It drives the program.
+	 * It reads in the data from a data file. Creates a new
+	 * Scores class object. Then calls thew new Scores
+	 * object's calculations and prints its data
+	 */
 	public static void main(String[] args) throws Exception
 	{
 		/* Uncomment if developing in Windows */
-		//File file = new File("C:\\Users\\rptho\\git\\Project1\\data\\Scores.txt");
+		File file = new File("C:\\Users\\rptho\\git\\Project1\\data\\Scores.txt");
 		/* Uncomment if developing in MacOS */
-		File file = new File("/Users/ryanthornton/eclipse-workspace/Project1/data/Scores.txt");
+		//File file = new File("/Users/ryanthornton/eclipse-workspace/Project1/data/Scores.txt");
 		BufferedReader br = new BufferedReader(new FileReader(file)); 
 		
-		String[] dates = new String[5];
-		double[][] scores = new double[5][24];
+		String[] dates = new String[5]; // make array to get dates
+		double[][] scores = new double[5][24]; // make array to get scores for each date
 		
-		String st;
-		int dateCount = 0;
-		int scoreCount = 0;
-		int scoreDay = 0;
+		String st; // stores value of read line
+		int dateCount = 0;  // control var date
+		int scoreCount = 0; // control var amount of scores
+		int scoreDay = 0;   // control var for day of scores
 		
+		/* while the line isn't null, parse the file and place data in respective arrays */
 		while ((st = br.readLine()) != null) 
 		{
 		  if (dateCount < 5) {
@@ -33,12 +50,13 @@ public class Project1Main
 			  }
 		  }
 		}
-		br.close();
+		br.close(); // close file
 		
-		Scores scoreData = new Scores(dates, scores);
-		scoreData.calculateMeans();
-		scoreData.calculateSD();
-		
+		Scores scoreData = new Scores(dates, scores); // make new Scores class object
+		scoreData.calculateMeans(); // calculate the means
+		scoreData.calculateSD(); // calculate the standard deviations
+		scoreData.tTest(); // compute the t test
+		/* after calculations, the data is printed */
 		scoreData.printData();
 	}
 
--- src/Scores.java
+++ src/Scores.java
@@ -1,60 +1,143 @@
+/************************************************************************* 
+  Student Name: Ryan Thornton
+  File Name: Scores.java
+  Project 1
+
+  This file is the Scores class. It contains private arrays for 
+ everything that needs to be calculated. It has functions to calculate
+ the needed variables, and a function to print all data.
+**************************************************************************/
 import java.lang.Math;
 
 public class Scores 
 {
-	private String[] dates = new String[5];
-	private double[][] scores = new double[5][24];
-	private double[] means = new double [5];
-	private double[] sd = new double[5];
+	private String[] dates = new String[5]; // private array for holding the dates from the data file
+	private double[][] scores = new double[5][24]; // private 2-D array for holding the scores for each date from the data file
+	private double[] means = new double [5]; // holds the mean for each day when calculateMeans() is called and run
+	private double[] sd = new double[5]; // holds the standard deviation for each day when calculateSD() is called and run
+	private char[][] ttest = new char[5][5]; // holds the t test results of Y or N when tTest() is called and run
 	
+	/*
+	 * These control variables prevent from hardcoding the loop value to a single number
+	 * everywhere. These values are known from the project requirements, and if the value 
+	 * changed, then it would be easier for the programmer to change this value for all 
+	 * loops used.
+	 * 
+	 * MAX_DATES - control variable for loops, 5 dates are read in from the data file
+	 * MAX_SCORES - control variable for loops, 24 scores for each date from the data file
+	 */
+	private int MAX_DATES = 5;
+	private int MAX_SCORES = 24;
+	
+	/*
+	 * Constructor for the Scores class which sets its private arrays dates and scores
+	 * and then initializes the ttest array with null values.
+	 * 
+	 *  @param - dateArray passes in the array of dates from the main which read the data file
+	 *  @param - scoreArray passes in the array of scores from the main which read the data file
+	 */
 	public Scores(String[] dateArray, double[][] scoreArray) 
 	{
 		dates = dateArray;
 		scores = scoreArray;
+		
+		/* fill ttest array w/ null values */
+		for (int i = 0; i < MAX_DATES; i++) {
+			for (int j = i + 1; j < MAX_DATES; j++) {
+				ttest[i][j] = ' ';
+			}
+		}
 	}
 	
+	/*
+	 * This function calculates the means of each date and
+	 * stores them in the private array means[]
+	 */
 	public void calculateMeans()
 	{
 		double tempSum = 0;
 		
-		for (int i = 0; i < 5; i ++) {
-			for (int j = 0; j < 24; j++) {
-				tempSum += scores[i][j];
+		/* Loop over each date, then in the nested loop goes over each data value for the date */
+		for (int i = 0; i < MAX_DATES; i ++) {
+			for (int j = 0; j < MAX_SCORES; j++) {
+				tempSum += scores[i][j]; // sums up the 24 data points into a temporary var of sum
 			}
-			means[i] = tempSum / 24;
-			tempSum = 0;
+			means[i] = tempSum / MAX_SCORES; // divides sum by the known amount of scores for each date
+			tempSum = 0; // zero out the sum for reuse
 		}
 	}
 	
+	/*
+	 * This function calculates the standard deviation for
+	 * each date and stores the SD in the privary array sd[]
+	 */
 	public void calculateSD()
 	{
 		double sum = 0;
 		double var1 = 0;
 		
-		for(int i = 0; i < 5; i++) {
-			for (int j = 0; j < 24; j++) {
-				var1 = (scores[i][j] - means[i]);
-				var1 = var1*var1;
-				sum += var1;
+		for(int i = 0; i < MAX_DATES; i++) {
+			for (int j = 0; j < MAX_SCORES; j++) {
+				var1 = (scores[i][j] - means[i]); // subtract the mean from the current data point
+				var1 = var1*var1; // square the data - mean
+				sum += var1; // add (data-mean)^2 to the total sum
 			}
-			sum = sum /24;
-			sd[i] = Math.sqrt(sum);
-			sum = 0;
+			sum = sum / MAX_SCORES; // divide the sum by amount of data (max scores = 24)
+			sd[i] = Math.sqrt(sum); // square root the sum / 24 and add to sd array
+			sum = 0; // zero out the sum for reuse
 		}
 	}
 	
+	/*
+	 * This function computes the t test of a date
+	 * against all previous dates before it.
+	 */
+	public void tTest()
+	{
+		// declare variables for calculation, init to zero
+		double sum1 = 0;
+		double sum2 = 0;
+		double Ssq = 0;
+		double denom = 0;
+		double t = 0;
+		
+		for (int i = 0; i < MAX_DATES; i++) {
+			for (int j = i + 1; j < MAX_DATES; j++) {
+				sum1 = (sd[i] * sd[i]) * MAX_SCORES; // use the previously calculated standard deviation to get sum of (xi - mu)^2
+				sum2 = (sd[j] * sd[j]) * MAX_SCORES; // ^same as before for sum2
+				
+				Ssq = (sum1 + sum2) / 46; // 46 = (24 + 24 - 2) - gets the common variance between the two dates (sums/46)
+				denom = 2 * (Ssq / MAX_SCORES); // denominator for the ttest calculated 
+				
+				t = (means[i] - means[j]) / (Math.sqrt(denom)); // calculate final t for t test
+				t = Math.abs(t); // take abs value of t
+				
+				/* if t > 2.25 set the ttest between these two dates to Y for yes sig. difference, else it's N for no */
+				if (t > 2.25) {
+					ttest[i][j] = 'Y';
+				} else {
+					ttest[i][j] = 'N';
+				}
+			}
+		}
+	}
+	
+	/*
+	 * This function prints the data and should be called
+	 * after all calculations are complete.
+	 */
 	public void printData()
 	{
 		/* print the headings */
 		System.out.printf("%-15s","Student");
-		for(int i = 0; i < 5; i++) {
-			System.out.printf("%-15s", dates[i]);
+		for(int i = 0; i < MAX_DATES; i++) {
+			System.out.printf("%-15s", dates[i]); // date for each heading
 		}
 		System.out.print("\n"); // new line after headings
 		
 		/* print the scores for each Student */
 		int studentNum = 1; // separate var for student number instead of using for loop counter
-		for(int i = 0; i < 24; i++) {
+		for(int i = 0; i < MAX_SCORES; i++) {
 			System.out.format("%-15d%-15.2f%-15.2f%-15.2f%-15.2f%-15.2f\n", studentNum, scores[0][i], scores[1][i], 
 					scores[2][i], scores[3][i], scores[4][i]);
 			studentNum++;
@@ -68,17 +151,34 @@ public class Scores
 		
 		/* print the means */
 		System.out.printf("%-15s", "Mean");
-		for(int i = 0; i < 5; i++) {
+		for(int i = 0; i < MAX_DATES; i++) {
 			System.out.printf("%-15.2f", means[i]);
 		}
 		System.out.print("\n");
 		
 		/* print the standard deviations */
 		System.out.printf("%-15s", "SD");
-		for(int i = 0; i < 5; i++) {
+		for(int i = 0; i < MAX_DATES; i++) {
 			System.out.printf("%-15.2f", sd[i]);
 		}
 		System.out.print("\n");
+		
+		/* Print the t test results in a formatted table */
+		System.out.print("\nSignificant Differences in Mean Scores\n"); // heading
+		System.out.printf("%-15s", ""); // buffer for y axis of dates
+		
+		for(int i = 1; i < MAX_DATES; i++) {
+			System.out.printf("%-15s", dates[i]); // print date headings
+		}
+		System.out.print("\n"); // new line after headings
+		
+		for (int i = 0; i < MAX_DATES; i++) {
+			System.out.printf("%-15s", dates[i]); // print current date
+			for (int j = 1; j < MAX_DATES; j++) {
+				System.out.format("%-15c", ttest[i][j]); // print current date's t test results
+			}
+			System.out.print("\n"); // new line after row
+		}
 	}
 	
 }
